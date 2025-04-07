dd33c5dd4143e3d40f31be9e77708b903611805b
Galina Lobacheva <147919859+KoshanSky1@users.noreply.github.com>
2025-04-07 15:21:50 +0300

    Refactor: errorHandler

--- src/main/java/cat_clinic_simulation/exception/ErrorHandler.java
+++ src/main/java/cat_clinic_simulation/exception/ErrorHandler.java
@@ -12,8 +12,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final EntityNotFoundException e) {
         return new ErrorResponse(
-                e.getMessage(),
-                HttpStatus.NOT_FOUND.value()
+                e.getMessage()
         );
     }
 
@@ -21,8 +20,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleNumberAlreadyExistsException(final NumberAlreadyExistsException e) {
         return new ErrorResponse(
-                e.getMessage(),
-                HttpStatus.CONFLICT.value()
+                e.getMessage()
         );
     }
 
@@ -30,8 +28,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleNameAlreadyExistsException(final NameAlreadyExistsException e) {
         return new ErrorResponse(
-                e.getMessage(),
-                HttpStatus.CONFLICT.value()
+                e.getMessage()
         );
     }
 
@@ -39,8 +36,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
         return new ErrorResponse(
-                e.getMessage(),
-                HttpStatus.BAD_REQUEST.value()
+                e.getMessage()
         );
     }
 
@@ -48,8 +44,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleOtherException(Exception e) {
         return new ErrorResponse(
-                e.getMessage(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value()
+                e.getMessage()
         );
     }
 
